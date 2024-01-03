c67ac050f7245b4c710ed44ad233969349f883a8
Shra1G <shravang49@gmail.com>
2024-01-03 17:59:25 +0530

    first commit

---
+++ Client/src/components/FlexBetween.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+});
+
+export default FlexBetween;
